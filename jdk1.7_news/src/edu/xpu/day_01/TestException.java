fce3346d4538192fda6c8380dc641a445687a8cf
zouchanglin <zchanglin@aliyun.com>
2019-10-29 10:27:04 +0800

    test gitignore file

--- jdk1.7_news/src/edu/xpu/day_01/TestException.java
+++ jdk1.7_news/src/edu/xpu/day_01/TestException.java
@@ -2,24 +2,24 @@ package edu.xpu.day_01;
 
 public class TestException {
 	
-	public static int fun(int i) {
-		int ret = 20;
-		try {
-			ret += 10;
-			int u = 10/i;
-			return ret;
-		}catch (Exception e) {
-			ret += 10;
-			e.printStackTrace();
-			return ret;
-		}finally {
-			return ret;
-		}
-	}
-	
-	public static void main(String[] args) {
-		int ret = fun(0);
-		System.out.println(ret);
-	}
+//	public static int fun(int i) {
+//		int ret = 20;
+//		try {
+//			ret += 10;
+//			int u = 10/i;
+//			return ret;
+//		}catch (Exception e) {
+//			ret += 10;
+//			e.printStackTrace();
+//			return ret;
+//		}finally {
+//			return ret;
+//		}
+//	}
+//	
+//	public static void main(String[] args) {
+//		int ret = fun(0);
+//		System.out.println(ret);
+//	}
 
 }
