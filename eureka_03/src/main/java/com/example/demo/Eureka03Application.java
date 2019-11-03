e9c6b41f444aa158b34a3a8de8a449257e335092
zouchanglin <zchanglin@aliyun.com>
2019-11-03 21:37:56 +0800

    add some ignore file

---
+++ eureka_03/src/main/java/com/example/demo/Eureka03Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eureka03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eureka03Application.class, args);
+	}
+
+}
--- maven_web_test/src/main/webapp/index.jsp
+++ maven_web_test/src/main/webapp/index.jsp
@@ -1,7 +1,7 @@
 <%@page pageEncoding="UTF-8" contentType="text/html; charset=UTF-8" %>
 <html>
 <body>
-<h1>测试修改成功</h1>
+<h1>测试修改成功！</h1>
 <h2>Hello World!</h2>
 </body>
 </html>
