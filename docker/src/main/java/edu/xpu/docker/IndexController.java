cde9a1c762f79ae2c1f9b5a96b17e6702355ab57
zouchanglin <zchanglin@aliyun.com>
2020-01-02 01:39:28 +0800

    IDEA集成Docker

---
+++ docker/src/main/java/edu/xpu/docker/IndexController.java
@@ -0,0 +1,18 @@
+package edu.xpu.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String index(){
+        //中文的注释是这个样子的
+        return "Hello Docker!!!<h1>Hello Docker!!!</h1>" +
+                "<h2>Hello Docker!!!</h2>" +
+                "<h3>Hello Docker!!!</h3>";
+    }
+}
